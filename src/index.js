8f79534629e69ab9214d00dc73af388eab79d099
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-23 14:54:33 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
- * @param {string | number | Date} date - The date to format. Can be a Date object, timestamp, or ISO string.
+ * @r | Date} date - The date to format. Can be a Date object, timestamp, or ISO string.
  * @returns {string} Human-readable relative time.
  */
 export function timeAgo(date) {
@@ -44,6 +44,7 @@ export function timeAgo(date) {
   // For anything beyond, return the number of years
   return `${years} years ago`;
 }
+
 
 
 
