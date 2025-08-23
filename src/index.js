54628665cff7a6eda2fb6fe04fbf36f6c9e96302
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-23 14:58:16 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-months (assuming ~30 days per month)
+ays per month)
   const months = Math.floor(days / 30);
   if (months === 1) return 'a month ago';
   if (months < 12) return `${months} months ago`;
@@ -10,6 +10,7 @@ months (assuming ~30 days per month)
   // For anything beyond, return the number of years
   return `${years} years ago`;
 }
+
 
 
 
