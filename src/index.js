a01ac83732c50160f97c69a4bafbb2e8bc688c2f
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-23 14:57:13 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-  if (hours === 1) return 'an hour ago';
+  if (hours
   if (hours < 24) return `${hours} hours ago`;
 
   // Calculate difference in days
@@ -19,6 +19,7 @@
   // For anything beyond, return the number of years
   return `${years} years ago`;
 }
+
 
 
 
