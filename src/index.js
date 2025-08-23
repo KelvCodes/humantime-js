216c04723e10fa59729ff4bba5168566cf3b4f1c
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-23 14:59:23 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
- 1]
-  // Approximate difference in years (assuming ~365 days per year)
+ears (assuming ~365 days per year)
   const years = Math.floor(days / 365);
   if (years === 1) return 'a year ago';
 
   // For anything beyond, return the number of years
   return `${years} years ago`;
 }
+
 
 
 
