7a5451b0755f78f5a722e699ac17176aea0a37e1
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-24 10:34:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 
 
-    const count = Math.floor(seco
-      return rtf.format(-count, unit); // negative count = "ago"
-    }
-  }
 
   return 'just now';
 }
@@ -14,6 +10,7 @@
 // console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 // console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 // console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (in French)
+
 
 
 
