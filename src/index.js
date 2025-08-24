5c121ddf126d267eca1a334a58e5083366f2d329
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-24 10:34:25 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
 
   return 'just now';
 }
-
-// ---------------- Example Usage ----------------
+------------ Example Usage ----------------
 // console.log(timeAgo(new Date(Date.now() - 45 * 1000)));   // "45 seconds ago"
 // console.log(timeAgo(new Date(Date.now() - 5 * 60000)));  // "5 minutes ago"
 // console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 // console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 // console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (in French)
+
 
 
 
