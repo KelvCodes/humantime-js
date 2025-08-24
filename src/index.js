f0b8003b7ef471d31401ecfb66828da95e9a92f6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-24 10:33:11 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 n seconds
-  const intervals = [
-    { unit: 'year', seconds: 31536000 },
+  const interseconds: 31536000 },
     { unit: 'month', seconds: 2592000 },
     { unit: 'day', seconds: 86400 },
     { unit: 'hour', seconds: 3600 },
@@ -25,6 +24,7 @@ n seconds
 // console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 // console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 // console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (in French)
+
 
 
 
