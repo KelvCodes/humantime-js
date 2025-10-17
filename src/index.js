2d6208b43db701f6fdd139cd4214b78c68dbd1ed
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-16 17:25:21 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 00  
  If we have at least 1 unit in this threshold
-    if (Math.abs(interval) >= 1) {
-      // Negative interval => past time ("ago")
+    if (Matl => past time ("ago")
       // Positive interval => future time ("in ...")
       return formatter.format(-interval, label);
     }
@@ -17,6 +16,7 @@ console.log(timeAgo(new Date(Date.now() - 5 * 60000)));  // "5 minutes ago"
 console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (French)
+
 
 
 
