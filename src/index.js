8904836a42feb8eb05ce19ca19a7b10e3bd604f6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-16 17:17:06 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-  * Examples:
+
  *   - "just now"
  *   - "5 minutes ago"
  *   - "yesterday"
@@ -62,6 +62,7 @@ console.log(timeAgo(new Date(Date.now() - 5 * 60000)));  // "5 minutes ago"
 console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (French)
+
 
 
 
