29e0ba41f6a7476eaa33ef11b4cd0faef81ef1fa
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-16 17:24:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-65 * 24 * 60 * 60 },  // 31536000    { label: 'day',    seconds: 24 * 60 * 60 },        // 86400
+60 },  // 31536000    { label: 'day',    seconds: 24 * 60 * 60 },        // 86400
     { label: 'hour',   seconds: 60 * 60 },             // 3600
     { label: 'minute', seconds: 60 },                  // 60
     { label: 'second', seconds: 1 }                    // 1
@@ -27,6 +27,7 @@ console.log(timeAgo(new Date(Date.now() - 5 * 60000)));  // "5 minutes ago"
 console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (French)
+
 
 
 
