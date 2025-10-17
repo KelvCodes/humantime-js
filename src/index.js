d6ab9c18292af3ccf2dfb9b6c113552ae3a1e3d3
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-16 17:20:44 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-ure the input is a valid Date object
+ valid Date object
 
   // Difference in seconds (can be negative for future dates)
   const diffInSeconds = Math.floor((now.getTime() - parsedDate.getTime()) / 1000);
@@ -39,6 +39,7 @@ console.log(timeAgo(new Date(Date.now() - 5 * 60000)));  // "5 minutes ago"
 console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (French)
+
 
 
 
