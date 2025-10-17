08e80013f5d1b51072da4f50fe54270ffd021351
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-16 17:19:39 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
-export funcfor empty input
-  if (!inputDate) {
+export funcfor empty
     return '';
   }
 
@@ -52,6 +51,7 @@ console.log(timeAgo(new Date(Date.now() - 5 * 60000)));  // "5 minutes ago"
 console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (French)
+
 
 
 
