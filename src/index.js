8773b7d355630fa3fe563a912c0761f7afec04f6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-16 17:20:25 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-Ensure the input is a valid Date object
+ure the input is a valid Date object
   co';
   }
 nt time
@@ -43,6 +43,7 @@ console.log(timeAgo(new Date(Date.now() - 5 * 60000)));  // "5 minutes ago"
 console.log(timeAgo(new Date(Date.now() - 86400000)));   // "yesterday"
 console.log(timeAgo(new Date(Date.now() + 3600000)));    // "in 1 hour"
 console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));      // "il y a 7 mois" (French)
+
 
 
 
