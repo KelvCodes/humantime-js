ce99940d016ed0c39070b1c698678d137e31166b
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-14 16:26:59 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
-
-export function timeAgo(
+function timeAgo(
   inputDate: string | number | Date,
   locale: string = 'en',
   short: boolean = false
@@ -73,6 +71,7 @@ console.log(timeAgo(new Date(Date.now() + 3600000)));       // "in 1 hour"
 console.log(timeAgo("2025-01-01T00:00:00Z", 'fr'));         // "il y a 7 mois"
 console.log(timeAgo(Date.now() - 7200000, 'en', true));     // "2h ago"
 console.log(timeAgo(Date.now() + 3 * 86400000, 'en', true)); // "in 3d"
+
 
 
 
