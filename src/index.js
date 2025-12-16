ea90da194d529805d0334eea152988cbb0129282
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-16 14:45:09 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-HORT_LABELS: Record<TimeAgoUnit, string> = {
+Record<TimeAgoUnit, string> = {
   year: 'y',
   month: 'mo',
   day: 'd',
@@ -60,6 +60,7 @@ export function timeAgo(
 
   return locale.startsWith('en') ? 'just now' : rtf.format(0, 'second');
 }
+
 
 
 
