69ea1ef0d94d54d93c08af7e20bf0ed95fc38576
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-16 14:37:44 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
-}[] = [
   { unit: 'year', seconds: 31536000 },
   { unit: 'month', seconds: 2592000 },
   { unit: 'day', seconds: 86400 },
@@ -70,6 +69,7 @@ export function timeAgo(
 
   return locale.startsWith('en') ? 'just now' : rtf.format(0, 'second');
 }
+
 
 
 
