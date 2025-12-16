7c9c9f630b3bd704ef19806103121f35424f4272
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-16 14:48:33 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-"
-      if (short) {
-        const label = SHORT_LABELS[unit];
-        return value > 0
-          ? `${Math.abs(value)}${label} ago`
-          : `in ${Math.abs(value)}${label}`;
-      }
 
-      // Localized long format
-      return rtf.format(-value, unit);
-    }
-  }
-
-  return locale.startsWith('en') ? 'just now' : rtf.format(0, 'second');
-}
 
 
 
