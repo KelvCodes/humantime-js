796a378af3822ecaa97e66161420ea20779a013d
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-18 21:32:46 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
-  locale?: string;
   short?: boolean;
   justNowThreshold?: number; // seconds
   maxUnit?: TimeAgoUnit; // largest unit to display
@@ -93,6 +92,7 @@ export function timeAgo(
 
   return locale.startsWith('en') ? 'just now' : rtf.format(0, 'second');
 }
+
 
 
 
