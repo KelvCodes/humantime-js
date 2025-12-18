f44a8e40cee6464f9ab33c6de1710842753d113a
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-18 21:31:43 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-type TimeAgoUnit = 'year' | 'month' | 'week' | 'day' | 'hour' | 'minute' | 'second';
+ TimeAgoUnit = 'year' | 'month' | 'week' | 'day' | 'hour' | 'minute' | 'second';
 
 interface TimeAgoOptions {
   locale?: string;
@@ -95,5 +95,6 @@ export function timeAgo(
 
   return locale.startsWith('en') ? 'just now' : rtf.format(0, 'second');
 }
+
 
 
