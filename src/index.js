d07467dd89b98f7990a7c932d0d9b736a84e9f4d
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-18 21:46:51 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-ls[unit]}`;
+
       } else {
         // Intl.RelativeTimeFormat expects negative for past
         return rtf.format(-value, unit);
@@ -8,6 +8,7 @@ ls[unit]}`;
 
   return locale.startsWith('en') ? 'just now' : rtf.format(0, 'second');
 }
+
 
 
 
