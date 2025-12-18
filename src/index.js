675a3dbb3e60391dc73102804dab1d157b201818
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-17 16:07:56 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-SHORT_LABELS: Record<TimeAgoUnit, string> = {
+ Record<TimeAgoUnit, string> = {
   year: 'y',
   month: 'mo',
   week: 'w',
@@ -76,6 +76,7 @@ export function timeAgo(
     ? 'just now'
     : rtf.format(0, 'second');
 }
+
 
 
 
