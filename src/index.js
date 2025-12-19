d0db41d563cccfd5dd45d76ffc372b0cfbde4758
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-19 11:16:14 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
- Partial<Record<TimeAgoUnit, string>>;
-}
+ Partial<Record<T}
 
 const TIME_UNITS = [
   { unit: 'year', seconds: 31_536_000 },
@@ -98,6 +97,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
    }
+
 
 
 
