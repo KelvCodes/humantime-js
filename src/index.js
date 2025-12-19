31c06904fac6e3012d6c1c5e4728bc7fe663c2ce
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-19 11:23:40 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-
-  TIME_UNITS.map((u, i) => [u.unit, i])
+(u, i) => [u.unit, i])
 ) as Record<TimeAgoUnit, number>;
 
 const DEFAULT_SHORT_LABELS: Readonly<Record<TimeAgoUnit, string>> = {
@@ -85,6 +84,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
    }
+
 
 
 
