17002c36f5de651b6cc201f31b0fb94d9d374329
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-19 11:23:08 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-DEX: Record<TimeAgoUnit, number> = Object.fromEntries(
+d<TimeAgoUnit, number> = Object.fromEntries(
   TIME_UNITS.map((u, i) => [u.unit, i])
 ) as Record<TimeAgoUnit, number>;
 
@@ -85,6 +85,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
    }
+
 
 
 
