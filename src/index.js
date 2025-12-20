7d7ddfc25483d29f506344539347256ee7302419
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-20 01:56:37 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
-  maxUnit?: TimeAgoUnit;
 
   /** Override short unit labels */
   shortLabels?: Partial<Record<TimeAgoUnit, string>>;
@@ -120,6 +119,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
 }
+
 
 
 
