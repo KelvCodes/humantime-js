804b7b1e20d3fbb42e5d3782c7854e9aae865436
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-21 00:43:33 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
- * Suitable for:
  *   UI feeds, timelines, logs, analytics, design systems, OSS libraries
  * ========================================================================== */
 
@@ -146,6 +145,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
 }
+
 
 
 
