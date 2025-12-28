d0b4388881a18abce9078937e4c43e6546bdb961
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2025-12-28 00:25:10 -0800

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
- } from '../src/index.js';
+dex.js';
 
 test('returns "just now" for very recent date', () => {
   expect(timeAgo(new Date())).toBe('just now');
@@ -18,5 +18,6 @@ test('returns "yesterday"', () => {
   const d = new Date(Date.now() - 24 * 60 * 60 * 1000); // 1 day ago
   expect(timeAgo(d)).toBe('yesterday');
 });
+
 
 
