cf17489575ad82c5a29e9b25d00a29d5a52410ed
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-05 03:22:34 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
-  /** Seconds considered as "just now" */
   justNowThreshold?: number;
 
   /** Largest unit allowed (e.g. 'day' => never show weeks/months) */
@@ -141,6 +140,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
 }
+
 
 
 
