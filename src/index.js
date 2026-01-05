078998b16b558aa0287d903f0be4fd493c3fdc77
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-05 03:03:53 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-r formatting
+
  *   dates as human-readable relative time (e.g. "just now", "2h ago", "in 3 days").
  *
  * Design goals:
@@ -170,6 +170,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
 }
+
 
 
 
