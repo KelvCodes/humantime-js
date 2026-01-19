d91d91a342a88f46e5cac790264f5f4518357623
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-19 09:34:12 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
 
-const TIME_UNITS = Object.freeze([
   { unit: 'year', seconds: 31_536_000 },
   { unit: 'month', seconds: 2_592_000 },
   { unit: 'week', seconds: 604_800 },
@@ -136,6 +135,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
 }
+
 
 
 
