8c610d6121a316b2a5070681bbdce5bbdabfd0f2
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-19 09:04:54 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
- *   • High-performance formatter caching
  *   • Framework-agnostic & tree-shakable
  * ========================================================================== */
 
@@ -194,6 +193,7 @@ export function timeAgo(
     ? 'just now'
     : getRTF(locale).format(0, 'second');
 }
+
 
 
 
